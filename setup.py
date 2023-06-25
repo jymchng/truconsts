b78c7e68adb0bcb197112ed359f9455fbcc068a9
Soon Siang <jymchng@hotmail.com>
2023-06-26 01:22:10 +0800

    all works

--- setup.py
+++ setup.py
@@ -9,38 +9,13 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-# ROOT_DIR = os.path.dirname(__file__)
-# PACKAGE_NAME = "truconsts"
-# sys.path.append(os.path.join(ROOT_DIR, PACKAGE_NAME))
-# CYTHON_DIR_NAME = "cy_src"
-# C_FILES_DIRECTORY = os.path.join(ROOT_DIR, CYTHON_DIR_NAME)
-
-
-# def generate_extensions(package_name, directory):
-#     extensions = []
-#     for root, dirs, files in os.walk(directory):
-#         print(root, dirs, files)
-#         for file in files:
-#             if file.endswith('.c'):
-#                 print(f"File ends with `.c`: {file}")
-#                 file_name = os.path.splitext(file)[0]
-#                 module_name = f"{package_name}.{file_name}"
-#                 print(f"`sources` = {CYTHON_DIR_NAME}/{file}")
-#                 extension = Extension(
-#                     module_name, sources=[f"{CYTHON_DIR_NAME}/{file}"])
-#                 extensions.append(extension)
-#     return extensions
-
-
-# ext_modules = generate_extensions(PACKAGE_NAME, C_FILES_DIRECTORY)
-# print("Extension modules are: ", ext_modules)
-
 for path in Path("src").glob("**/*.c"):
     print(
         """str(path.relative_to("src").with_suffix("")).replace(os.sep, ".") = """, str(
             path.relative_to("src").with_suffix("")).replace(
             os.sep, "."))
     print("sources = ", [str(path)])
+
 
 setup(
     ext_modules=[
@@ -56,10 +31,3 @@ setup(
     py_modules=[path.stem for path in Path("src").glob("*.py")],
     include_package_data=True,
 )
-
-
-# if __name__ == '__main__':
-#     print(f"ROOT_DIR={ROOT_DIR}")
-#     print(f"PACKAGE_NAME={PACKAGE_NAME}")
-#     print(f"CYTHON_DIR_NAME={CYTHON_DIR_NAME}")
-#     print(f"C_FILES_DIRECTORY={C_FILES_DIRECTORY}")
--- src/truconsts/annotations.py
+++ src/truconsts/annotations.py
@@ -2,23 +2,30 @@ from typing import Type, Tuple
 from types import MethodType
 
 
+class InvalidClassSubscripts(BaseException):
+    ...
+
+
+class InstantiationNotAllowed(BaseException):
+    ...
+
+
 def patch_cls(cls):
     def __cls_getitem__(cls, index):
         if isinstance(index, (Type, Tuple)
                       ) or index == Cache or index == Yield:
             res = (cls, *index) if isinstance(index, tuple) else (cls, index)
             if ((Cache in res) and (Yield in res)):
-                raise Exception(
-                    f"Subscripts cannot contain both `Cache` and `Yield`")
+                raise InvalidClassSubscripts(
+                    f"Class Subscripts cannot contain both `Cache` and `Yield`")
             return res
-        raise Exception(f"`{index}` is not a type or tuple(type) or `Cache`")
+        raise InvalidClassSubscripts(
+            f"`{index}` is not a type or tuple(type) or `Cache` or `Yield` or a class-subscripted combination of `Cache` and `Yield`")
 
     def __init__(self, *args, **kwargs):
-        raise Exception(f"`{self.__name__}` cannot be instantiated")
-    # def __init_subclass__(self, *args, **kwargs):
-    #     raise Exception(f"`{self.__name__}` cannot be subclassed")
+        raise InstantiationNotAllowed(
+            f"`{self.__name__}` cannot be instantiated")
     cls.__init__ = MethodType(__init__, cls)
-    # cls.__init_subclass__ = MethodType(__init_subclass__, cls)
     cls.__class_getitem__ = MethodType(__cls_getitem__, cls)
     return cls
 
